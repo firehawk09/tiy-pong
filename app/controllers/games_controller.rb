d70bdf3f64d187b7fc928c6791744d309c357ed3
vdolbilov <vitdolb@gmail.com>
2015-02-02 00:35:19 -0600

    removed game destroy/create paths

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -24,17 +24,7 @@ class GamesController < ApplicationController
   # POST /games
   # POST /games.json
   def create
-    @game = Game.new(game_params)
-
-    respond_to do |format|
-      if @game.save
-        format.html { redirect_to @game, notice: 'Game was successfully created.' }
-        format.json { render :show, status: :created, location: @game }
-      else
-        format.html { render :new }
-        format.json { render json: @game.errors, status: :unprocessable_entity }
-      end
-    end
+    redirect_to games_path
   end
 
   # PATCH/PUT /games/1
@@ -54,11 +44,7 @@ class GamesController < ApplicationController
   # DELETE /games/1
   # DELETE /games/1.json
   def destroy
-    @game.destroy
-    respond_to do |format|
-      format.html { redirect_to games_url, notice: 'Game was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to games_path
   end
 
   private
